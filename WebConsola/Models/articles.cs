1323693545c7cc0573c1ecc0a3fa1c2b5683240f
Reghis Villarroel Roca <reghisbot@gmail.com>
2020-10-17 15:46:01 -0300

    Inicio de Proyecto SuperZapato

---
+++ WebConsola/Models/articles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebConsola.Models
+{
+    public class articles
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public decimal total_in_shelf { get; set; }      // despensa
+        public decimal total_in_vault { get; set; }     // bodega
+        public int store_id { get; set; }
+    }
+}
